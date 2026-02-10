9889e56e99761f6f9821321e06dfa166772cb282
docujun <changjunbeeez@gmail.com>
2026-02-11 01:51:23 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// PWA 서비스 워커 등록
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js').catch(error => {
